c76496f73f1d103e1e33599946df6848bc3e5745
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-23 22:11:49 -0500

    Tried to read past array bounds in order to generate an exception.

--- src/chapter11/Main.java
+++ src/chapter11/Main.java
@@ -3,6 +3,13 @@ package chapter11;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] numArray;
+	    BadArray ba = new BadArray();
+	    numArray = ba.getNumbers();
+
+	    // Attempt to read past the three int array
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(numArray[i]);
+        }
     }
 }
