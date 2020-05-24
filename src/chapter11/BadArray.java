f7ffd08a6ef2aeba3ad8517722d5ae53b015f0b2
Roberto Aguinaga <raguinaga1@outlook.com>
2020-05-23 21:58:11 -0500

    Created a class that creates and gets an array.

---
+++ src/chapter11/BadArray.java
@@ -0,0 +1,11 @@
+package chapter11;
+
+public class BadArray {
+
+    //Create an int array
+    private int[] numbers = {1,2,3};
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+}
